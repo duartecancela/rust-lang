add92aebca2229593370d3d381ae5638836425d2
Duarte Cancela <duartecancela@gmail.com>
2021-03-06 22:10:31 +0000

    update gitignore

--- guessing_game/target/debug/deps/guessing_game-5ac6566481573f12.d
+++
@@ -1,5 +0,0 @@
-/home/duartec/eda2020/rust/rust-lang/guessing_game/target/debug/deps/guessing_game-5ac6566481573f12: src/main.rs
-
-/home/duartec/eda2020/rust/rust-lang/guessing_game/target/debug/deps/guessing_game-5ac6566481573f12.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/duartec/eda2020/rust/rust-lang/guessing_game/target/debug/guessing_game: /home/duartec/eda2020/rust/rust-lang/guessing_game/src/main.rs
